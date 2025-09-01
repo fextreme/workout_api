f0ca85a30cfbc9a5ddc08910d99382a0c9e6a492
fextreme <84205138+fextreme@users.noreply.github.com>
2025-08-31 21:12:50 -0300

    Update main.py

--- workout_api/main.py
+++ workout_api/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
-from workout_api.routers import api_router
+from fastapi_pagination import add_pagination
+from workout_api.routers import api_router  # Router central que inclui todos os módulos
 
-app = FastAPI(title='WorkoutApi')
+app = FastAPI(title="Workout API")
+
+# Registrar o router principal
 app.include_router(api_router)
+
+# Adicionar paginação global
+add_pagination(app)
+
