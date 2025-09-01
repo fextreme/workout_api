2531c1a14e8a5be59cc5309f5a16d49c0eca4d2c
fextreme <84205138+fextreme@users.noreply.github.com>
2025-08-31 21:13:32 -0300

    Update database.py

--- workout_api/configs/database.py
+++ workout_api/configs/database.py
@@ -1,15 +1,19 @@
 from typing import AsyncGenerator
-
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from workout_api.configs.settings import settings
 
-
+# Criar engine assíncrona
 engine = create_async_engine(settings.DB_URL, echo=False)
+
+# Criar sessionmaker assíncrono
 async_session = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
 )
 
-async def get_session() -> AsyncGenerator:
+# Função para injetar sessão no FastAPI
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
